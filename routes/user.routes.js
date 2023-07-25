8ce36e4857f3d3a5896c4e6428de04850c235dc9
Muhammad Juan Pradana <30497994+juanpradana@users.noreply.github.com>
2023-07-25 12:29:48 +0700

    add create directory when not available

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,29 @@
 const multer = require('multer');
 const express = require('express');
+const fs = require('fs');
 
 const router = express.Router();
 const controller = require('../controllers/user.controller');
 
+const edCov1Dir = 'eddyCov1';
+const edCov2Dir = 'eddyCov2';
+
+const createDirectoryIfNotExist = (directoryName) => {
+  if (!fs.existsSync(directoryName)) {
+    fs.mkdirSync(directoryName);
+    console.log(`Direktori ${directoryName} berhasil dibuat.`);
+  } else {
+    console.log(`Direktori ${directoryName} sudah ada.`);
+  }
+};
+
+createDirectoryIfNotExist(edCov1Dir);
+createDirectoryIfNotExist(edCov2Dir);
+
 // Set up multer for file upload
 const storageUpED1 = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'eddyCov1/');
+    cb(null, edCov1Dir);
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
@@ -15,7 +31,7 @@ const storageUpED1 = multer.diskStorage({
 });
 const storageUpED2 = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'eddyCov2/');
+    cb(null, edCov2Dir);
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
